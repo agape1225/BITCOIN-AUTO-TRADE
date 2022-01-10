fc601c2e1af0feabc490ad3009959dcb4779a971
agape1225 <49804691+agape1225@users.noreply.github.com>
2022-01-10 23:31:56 +0900

    Update config.ini.py

--- conf/config.ini.py
+++ conf/config.ini.py
@@ -4,5 +4,3 @@ client_id = "moHQ44PqqLYU64oUjiQdLz7bzErnCNyIhNDOxkQ5tovZDRodTnmxfiFykTf7b"
 client_secret = "6dN3CUYf4DeolRh7EldrGvvFOFtjQ0ZG9XsvQdTWQq1CELdOvwefCaAJ3QCkr"
 username = "scg9268@naver.com"
 password = "agape1225!"
-
-BITCOIN-AUTO-TRAD/conf/config.ini.py 
