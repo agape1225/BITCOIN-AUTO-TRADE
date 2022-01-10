0f6fa430482d176d3c4affa4fe4cf4d3c7573d82
agape1225 <49804691+agape1225@users.noreply.github.com>
2022-01-10 23:28:38 +0900

    Update config.ini.py

--- conf/config.ini.py
+++ conf/config.ini.py
@@ -1,5 +1,5 @@
 ['KORBIT']
-#this is kobit
+#this is kobits
 client_id = "moHQ44PqqLYU64oUjiQdLz7bzErnCNyIhNDOxkQ5tovZDRodTnmxfiFykTf7b"
 client_secret = "6dN3CUYf4DeolRh7EldrGvvFOFtjQ0ZG9XsvQdTWQq1CELdOvwefCaAJ3QCkr"
 username = "scg9268@naver.com"
