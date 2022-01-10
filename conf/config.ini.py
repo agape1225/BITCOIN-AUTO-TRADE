8469c27cbd078b95dbb12fde2c75283085c58bd3
agape1225 <49804691+agape1225@users.noreply.github.com>
2022-01-10 23:30:15 +0900

    Delete config.ini.py

--- conf/config.ini.py
+++
@@ -1,6 +0,0 @@
-['KORBIT']
-#this is kobits
-client_id = "moHQ44PqqLYU64oUjiQdLz7bzErnCNyIhNDOxkQ5tovZDRodTnmxfiFykTf7b"
-client_secret = "6dN3CUYf4DeolRh7EldrGvvFOFtjQ0ZG9XsvQdTWQq1CELdOvwefCaAJ3QCkr"
-username = "scg9268@naver.com"
-password = "agape1225!"
