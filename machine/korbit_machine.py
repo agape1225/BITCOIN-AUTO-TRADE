6c6b0b70ec1a39586201369e7469be1176c372da
agape1225 <49804691+agape1225@users.noreply.github.com>
2022-01-12 01:13:19 +0900

    update get token method

--- machine/korbit_machine.py
+++ machine/korbit_machine.py
@@ -1,5 +1,7 @@
 import configparser
 
+from pip._vendor import requests
+
 from machine.base_machine import Machine
 
 
@@ -17,9 +19,65 @@ class KorbitMachine(Machine):
         config = configparser.ConfigParser()
         config.read('conf/config.ini')
         self.CLIENT_ID = config['KORBIT']['client_id']
-        self.CLIENT_SECRET  = config['KORBIT']['client_secret']
+        self.CLIENT_SECRET = config['KORBIT']['client_secret']
         self.USER_NAME = config['KORBIT']['username']
         self.PASSWORD = config['KORBIT']['password']
-        self.assess_token = None
+        self.access_token = None
         self.refresh_token = None
         self.token_type = None
+
+    def set_token(self, grant_type="password"):
+        """
+        엑세스 토큰 정보를 만들기 위한 메서드.
+
+        :param grant_type: 비밀번호
+
+        :return:
+        만료시간(expire), 엑세스 토큰(access_token), 리프레시 토큰(refresh_token)을 반환한다.
+
+        :raise:
+        grant_type이 password나 refresh_token이 아닌 경우 Exception을 발생시킨다.
+        """
+
+        token_api_path = "https://api.korbit.co.kr/v1/oauth2/access_token"
+        url_path = self.BASE_API_URL + token_api_path
+
+        if grant_type == "password":
+
+            data = {"client_id": self.CLIENT_ID,
+                    "client_secret": self.CLIENT_SECRET,
+                    "username": self.USER_NAME,
+                    "password": self.PASSWORD,
+                    "grant_type": grant_type}
+
+        elif grant_type == 'refresh_token':
+
+            data = {"client_id": self.CLIENT_ID,
+                    "client_secret": self.CLIENT_SECRET,
+                    "refresh_token": self.refresh_token,
+                    "grant_type": grant_type}
+
+        else:
+            raise Exception("Unexpected grant+type")
+
+        res = requests.post(url_path, data=data)
+        result = res.json()
+        self.access_token = result['access_token']
+        self.token_type = result['token_type']
+        self.refresh_token = result['refresh_token']
+        self.expire = result['expires_in']
+        return self.expire, self.assess_token, self.refresh_token
+
+    def get_token(self):
+       """
+       엑세스 토큰 정보를 받기 위한 메소드입니다.
+       :return:
+       엑세스 토큰(access_token)이 있는 경우 반환합니다.
+       :raise
+       access_token이 없는 경우 Exception을 발생시킵니다.
+       """
+       if self.access_token is not None:
+           return self.access_token
+       else:
+           raise Exception("Need to set_token")
+
--- tests/test_korbit_machine.py
+++ tests/test_korbit_machine.py
@@ -4,8 +4,17 @@ import inspect
 
 class KorbitMachineTestCase(unittest.TestCase):
 
-    def setUp(self):
+    def __init__(self):
         self.korbit_machine = KorbitMachine()
 
     def tearDown(self):
         pass
+
+    def test_set_token(self):
+        print(inspect.stack()[0][3])
+        expire, access_token, refresh_token = self.korbit_machine.set_token(grant_type="password")
+        assert access_token
+        print("Expire: ", expire, "Acceess_token: ", access_token, "Refresh_token: ", refresh_token)
+
+buf = KorbitMachineTestCase()
+buf.test_set_token()
