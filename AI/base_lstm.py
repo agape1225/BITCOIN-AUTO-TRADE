fdfa9fcf534079a0333c1716da0145cfd590d438
agape1225 <scg9268@gmail.com>
2023-10-29 03:45:19 +0900

    ✨ create test data
    Chat GPT를 위한 TEST 데이터 생성 완료

--- AI/base_lstm.py
+++ AI/base_lstm.py
@@ -15,13 +15,29 @@ data = mongodbMachine.find_items(db_name="AI", collection_name="actual_data")
 data = list(data)
 #data = extract_close_prices(data)
 result = []
-for i in range(len(data)-15, -1, -15):
+for i in range(0, len(data) - 15):
     chunk = data[i:i+15]
     result.insert(0, chunk)
-
-
+result.reverse()
 for i in result:
     print(i)
+
+tmp = []
+for i in result:
+    print(i)
+    data = extract_close_prices(i)
+    data = lstmMachine.data_processing(data)
+    result = lstmMachine.get_predict_value(data)
+    one_day_data = {}
+    date_string = i[-1]["timestamp"]  # 예시로 사용할 날짜 문자열
+    date_format = "%Y-%m-%d"  # 날짜 형식을 지정합니다. 여기서는 "년-월-일" 형식입니다.
+    one_day_data["timestamp"] = ( datetime.datetime.strptime(date_string, date_format) + datetime.timedelta(days=1)).strftime("%Y-%m-%d")
+    one_day_data["predicted_price"] = result + 0.0
+    #print(one_day_data)
+    mongodbMachine.insert_item(data=one_day_data, db_name="AI", collection_name="predicted_data")
+
+# for i in tmp:
+#     print(i)
 
 # #하루치 데이터 저장
 # data = bithumbMachine.get_last_data()
